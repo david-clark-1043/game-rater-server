f0725ebb61fcca3c2c9639b542b747d1f986d2ca
David Clark <david.clark.1043@gmail.com>
2022-05-09 11:42:39 -0500

    Added create functionality for images, reviews, and ratings

--- gamerater/urls.py
+++ gamerater/urls.py
@@ -4,10 +4,15 @@ from django.urls import path
 from rest_framework import routers
 from gameraterapi.views import GameView, CategoryView
 from gameraterapi.views import register_user, login_user
+from gameraterapi.views.image import ImageView
+from gameraterapi.views.rating import RatingView
+from gameraterapi.views.review import ReviewView
 
 router = routers.DefaultRouter(trailing_slash=False)
 router.register(r'categories', CategoryView, 'category')
-# router.register(r'events', EventView, 'event')
+router.register(r'reviews', ReviewView, 'review')
+router.register(r'ratings', RatingView, 'rating')
+router.register(r'images', ImageView, 'rating')
 router.register(r'games', GameView, 'game')
 
 
--- gameraterapi/views/__init__.py
+++ gameraterapi/views/__init__.py
@@ -1,4 +1,6 @@
 from .auth import login_user, register_user
 from .category import CategoryView
-# from .event import EventView
+from .rating import RatingView
+from .review import ReviewView
+from .image import ImageView
 from .game import GameView
